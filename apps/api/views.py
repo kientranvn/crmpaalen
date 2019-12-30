df807f50065dcec0dea52835d4543a1a0ebfd79a
Kien Tran <kientran@paalen.com>
2019-12-30 23:44:31 +0700

    Add Django rest_framework

--- apps/api/views.py
+++ apps/api/views.py
@@ -1,14 +1,23 @@
-// This lib will be export the json format
 from django.http import JsonResponse
 
 from django.shortcuts import render
 
 
+# third party imports
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-def test_view(request):
-    data = {
-        'name': 'Kien',
-        'age': 25
-    }
-    
-    return JsonResponse(data)
+class TestView(APIView):
+    def get(self, request, *agrs, **kwargs):
+        data = {
+            'name': 'Kien',
+            'age': 25
+        }    
+        return Response(data)
+
+#def test_view(request):
+#    data = {
+#        'name': 'Kien',
+#        'age': 25
+#    }    
+#    return JsonResponse(data)
