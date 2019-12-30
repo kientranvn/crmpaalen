7679280b0cf9a5db3faf21d49e279b837474bebe
Kien Tran <kientran@paalen.com>
2019-12-31 00:20:12 +0700

    Create serializers

--- apps/api/views.py
+++ apps/api/views.py
@@ -7,13 +7,28 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from .serializers import PostSerializer
+from .models import Post
+
+
 class TestView(APIView):
     def get(self, request, *agrs, **kwargs):
-        data = {
-            'name': 'Kien',
-            'age': 25
-        }    
-        return Response(data)
+        qs = Post.objects.all()
+        #serializers = PostSerializer(qs, many=True)
+        post = qs.first()
+        serializers = PostSerializer(post)
+        return Response(serializers.data )
+
+    def post(self, request, *args, **kwargs):
+        serializers = PostSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data)
+        return Response(serializers.errors)
+
+
+
+
 
 #def test_view(request):
 #    data = {
